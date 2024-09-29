e892309eada341f0f23e6e61e49088f907a6c5f6
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-09-29 13:13:16 +0530

    career confusion

---
+++ src/component/CareerConfusion.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+const CareerRecommendation = () => {
+    const navigate = useNavigate();
+    function handleClick(){
+      navigate('/ExpertCall')
+    }
+  return (
+    <Container fluid className="text-center py-5" style={{ backgroundColor: ' #FEF0E3' }}>
+      <Row className="justify-content-center">
+        <Col md={8} lg={6}>
+          <h2>If you are 11th,12th  and confused about the best career choice,talk to us</h2>
+          <p style={{fontSize:"20px",fontWeight:"400"}}>
+            Discover your path with college.Explore your interest in various graduation streams and get advice guidance free.
+          </p>
+          <Button variant="warning" size="lg" style={{backgroundColor:" #F87217",border:"none"}} onClick={handleClick}>Get your path</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default CareerRecommendation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../component/Navbar'
 import Slider from '../component/Slider'
 import SelectGoal from '../component/SelectGoal'
 import CollegeBaanner from '../component/CollegeBanner'
+import CareerConfusion from '../component/CareerConfusion'
 import Footer from '../component/Footer'
 function Home() {
   return (
@@ -10,7 +11,9 @@ function Home() {
     <Navbar></Navbar>
     <Slider></Slider>
     <SelectGoal></SelectGoal>
+    
     <CollegeBaanner></CollegeBaanner>
+    <CareerConfusion></CareerConfusion>
     <Footer></Footer>
    
     
