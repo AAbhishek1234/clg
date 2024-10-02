a40c8db10354917c67a5ebe80275f740b0b2e540
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-02 22:31:43 +0530

    try for admin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from './pages/Home'
 import ApplicationForm from './component/ApplicatonForm';
 import ExpertCall from './component/ExpertCall'
-import Admin from './pages/Admin'
+import AdminDashboard from './pages/AdminDashboard'
 import Login from './pages/Login'
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
     <Route path='/' element={<Home/>}> </Route>
     <Route path='/ApplicationForm' element={<ApplicationForm/>}/>
     <Route path='/ExpertCall' element={<ExpertCall/>}/>
-    <Route path='/admin/dashboard'element={<Admin/>}/>
+    <Route path='/admin/dashboard'element={<AdminDashboard/>}/>
     <Route path='/Login' element={<Login/>}/>
     </Routes>
         </Router>
